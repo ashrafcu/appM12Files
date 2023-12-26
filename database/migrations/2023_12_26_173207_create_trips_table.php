04196bcd152e280c37fd5df362a6f5f7c5f7e54a
Ashraf Ahmed <ahmashraf@gmail.com>
2023-12-27 00:06:51 +0600

    More Migration Files

---
+++ database/migrations/2023_12_26_173207_create_trips_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('starting_point', 50);
+            $table->string('ending_point', 50);
+            $table->integer('passengers');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
