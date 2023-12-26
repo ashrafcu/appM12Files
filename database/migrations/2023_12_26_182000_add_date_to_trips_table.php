c36017bae15b246b101130b0f121f552466aba15
Ashraf Ahmed <ahmashraf@gmail.com>
2023-12-27 00:29:00 +0600

    Migration Changes

---
+++ database/migrations/2023_12_26_182000_add_date_to_trips_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trips', function (Blueprint $table) {
+            $table->after('ending_point', function (Blueprint $table) {
+                $table->date('trip_date');
+                $table->time('trip_time');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trips', function (Blueprint $table) {
+            //
+        });
+    }
+};
